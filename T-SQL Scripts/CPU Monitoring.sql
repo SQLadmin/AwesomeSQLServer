b1a0b12af51c2334ed4b3ad79df59aec0887f4f2
SqlAdmin <r.bhuvanesh@outlook.com>
2017-04-22 19:36:41 +0530

    Update CPU Monitoring.sql

--- T-SQL Scripts/CPU Monitoring.sql
+++ T-SQL Scripts/CPU Monitoring.sql
@@ -216,3 +216,35 @@ FROM   sys.dm_exec_query_stats AS qs
 ORDER  BY qs.total_worker_time DESC 
 OPTION (recompile); 
 
+
+-------------------------------------------------------------
+/*** 5.TOP costliest Queries with batch and more details ***/
+-------------------------------------------------------------
+
+SELECT TOP 50
+  [Avg. MultiCore/CPU time(sec)] = qs.total_worker_time / 1000000 / qs.execution_count,
+  [Total MultiCore/CPU time(sec)] = qs.total_worker_time / 1000000,
+  [Avg. Elapsed Time(sec)] = qs.total_elapsed_time / 1000000 / qs.execution_count,
+  [Total Elapsed Time(sec)] = qs.total_elapsed_time / 1000000,
+  qs.execution_count,
+  [Avg. I/O] = (total_logical_reads + total_logical_writes) / qs.execution_count,
+  [Total I/O] = total_logical_reads + total_logical_writes,
+  Query = SUBSTRING(qt.[text], (qs.statement_start_offset / 2) + 1,
+  (
+  (
+  CASE qs.statement_end_offset
+    WHEN -1 THEN DATALENGTH(qt.[text])
+    ELSE qs.statement_end_offset
+  END - qs.statement_start_offset
+  ) / 2
+  ) + 1
+  ),
+  Batch = qt.[text],
+  [DB] = DB_NAME(qt.[dbid]),
+  qs.last_execution_time,
+  qp.query_plan
+FROM sys.dm_exec_query_stats AS qs
+CROSS APPLY sys.dm_exec_sql_text(qs.[sql_handle]) AS qt
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) AS qp
+
+ORDER BY [Total MultiCore/CPU time(sec)] DESC;
